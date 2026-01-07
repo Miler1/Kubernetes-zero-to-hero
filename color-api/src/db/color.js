52b9f0489866c26efa3563a55b31833a9fcf6d83
Miler1 <milergrudtner@gmail.com>
2026-01-06 23:20:21 -0300

    Add files

---
+++ color-api/src/db/color.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const ColorSchema = new mongoose.Schema({
+    key: String,
+    value: String
+});
+
+const Color = mongoose.model('Color', ColorSchema);
+
+const saveColor = async ({ key, value }) => {
+
+    let color = await Color.findOne({ key });
+
+    if (color) {
+        color.set({ value });
+    } else {
+        color = new Color({ key, value })
+    }
+
+    await color.save();
+
+};
+
+const getColor = async ({ key, strict = false }) => {
+    let color = await Color.findOne({ key }); // key, value
+
+    if (strict && !color) {
+        return undefined;
+    }
+    if (color) {
+        return color.value;
+    } 
+
+    return process.env.DEFAULT_COLOR || 'blue';
+}
+
+const getColors = async () => Color.find();
+
+const deleteColor = async ({ key }) => Color.deleteOne({ key });
+
+module.exports = {
+    saveColor,
+    getColor,
+    getColors,
+    deleteColor
+} 
